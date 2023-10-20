d117075dc45aef8438cdd2f2690b51c957b0c56c
fernandhardoni <fernandhardoni33@gmail.com>
2023-10-20 11:05:22 +0700

    v1.3

--- proses.php
+++ proses.php
@@ -1,1 +1,15 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $operand1 = $_POST["operand1"];
+    $operand2 = $_POST["operand2"];
+    $operator = $_POST["operator"];
+    
+    if ($operator == "+") {
+        $result = $operand1 + $operand2;
+    } else {
+        // error
+    }
+    
+    echo "Hasil: " . $result;
+}
+?>
