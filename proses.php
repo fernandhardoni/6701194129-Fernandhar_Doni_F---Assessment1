fb83824ef492e5e9309bb7512502b40ff315af64
fernandhardoni <fernandhardoni33@gmail.com>
2023-10-20 11:06:46 +0700

    v1.2.1

--- proses.php
+++ proses.php
@@ -1,1 +1,17 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $operand1 = $_POST["operand1"];
+    $operand2 = $_POST["operand2"];
+    $operator = $_POST["operator"];
+    
+    switch ($operator) {
+        case "+":
+            $result = $operand1 + $operand2;
+            break;
+        default:
+            // Handle other operators here
+    }
+    
+    echo "Hasil: " . $result;
+}
+?>
